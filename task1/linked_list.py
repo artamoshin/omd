a1fdd384ef71dfe371e681732b5c7e556719bd06
Alexander Artamoshin <artamoshin@gmail.com>
2019-08-13 02:25:34 +0300

    Задание 1: Реализация класса связного списка + тесты

---
+++ task1/linked_list.py
@@ -0,0 +1,86 @@
+class Element:
+    __slots__ = ('value', 'next')
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class ListIterator:
+    def __init__(self, collection):
+        self.cursor = collection.head
+
+    def __next__(self):
+        if self.cursor is None:
+            raise StopIteration
+
+        value = self.cursor.value
+        self.cursor = self.cursor.next
+        return value
+
+
+class Stack:
+    def __init__(self):
+        self.last = None
+
+    def push(self, value):
+        element = Element(value)
+
+        if self.last is not None:
+            element.next = self.last
+
+        self.last = element
+
+    def pop(self):
+        if self.last is None:
+            raise IndexError("pop from empty stack")
+
+        value = self.last.value
+        self.last = self.last.next
+        return value
+
+    def pop_all(self):
+        while self.last is not None:
+            yield self.pop()
+
+
+class List:
+    def __init__(self, *args):
+        self.head = None
+        self.tail = None
+
+        for value in args:
+            self.append(value)
+
+    def append(self, value):
+        element = Element(value)
+
+        if self.head is None:
+            self.head = element
+
+        if self.tail is not None:
+            self.tail.next = element
+
+        self.tail = element
+
+    def __iter__(self):
+        return ListIterator(self)
+
+    def print(self):
+        return ' '.join(map(str, self))
+
+    def __str__(self):
+        values = ', '.join(map(str, self))
+        return f'<{values}>'
+
+    def __iadd__(self, other):
+        for value in other:
+            self.append(value)
+        return self
+
+    def print_reversed(self):
+        stack = Stack()
+        for value in self:
+            stack.push(value)
+
+        return ' '.join(map(str, stack.pop_all()))
---
+++ task1/tests.py
@@ -0,0 +1,61 @@
+from unittest import TestCase
+
+from .linked_list import List, Stack
+
+
+class TestList(TestCase):
+    def test_samples(self):
+        list_ = List(1, 2, 3)
+        self.assertEqual(list_.print(), '1 2 3')
+
+        list_.append(4)
+        self.assertEqual(list_.print(), '1 2 3 4')
+
+        tail = List(5, 6)
+        list_ += tail  # shallow copy, see examples below
+        self.assertEqual(list_.print(), '1 2 3 4 5 6')
+
+        tail.head.value = 0
+        self.assertEqual(tail.print(), '0 6')  # element 5 in tail is changed
+        self.assertEqual(list_.print(), '1 2 3 4 5 6')  # element 5 in list_ is NOT changed
+
+        list_ += [7, 8]
+        self.assertEqual(list_.print(), '1 2 3 4 5 6 7 8')
+        list_ += ()
+        self.assertEqual(list_.print(), '1 2 3 4 5 6 7 8')
+
+    def test_iteration(self):
+        list_ = List(*range(1, 9))
+        self.assertEqual([2 ** elem for elem in list_], [2, 4, 8, 16, 32, 64, 128, 256])
+
+    def test_print_reversed(self):
+        list_ = List(*range(1, 9))
+        self.assertEqual(list_.print_reversed(), '8 7 6 5 4 3 2 1')
+
+    def test_empty_list(self):
+        empty_list = List()
+        self.assertEqual(empty_list.print(), '')
+
+    def test_none_value(self):
+        list_with_single_none_element = List(None)
+        self.assertEqual(list_with_single_none_element.print(), 'None')
+
+
+class TestStack(TestCase):
+    def test_samples(self):
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        self.assertEqual(stack.pop(), 3)
+        self.assertEqual(stack.pop(), 2)
+        self.assertEqual(stack.pop(), 1)
+        with self.assertRaises(IndexError):
+            stack.pop()
+
+    def test_pop_all(self):
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        self.assertEqual(list(stack.pop_all()), [3, 2, 1])
